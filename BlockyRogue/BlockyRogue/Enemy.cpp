d61e08241d785659418bb9227555700947ef1941
Adam L. More <AdamLMore@users.noreply.github.com>
2014-01-18 15:08:30 -0800

    Added a global config file Config.h to be used for designers.

--- BlockyRogue/BlockyRogue/Enemy.cpp
+++ BlockyRogue/BlockyRogue/Enemy.cpp
@@ -3,9 +3,11 @@
 
 Enemy::Enemy()
 {
+	sf::VideoMode currentResolution = sf::VideoMode::getDesktopMode();
     std::cout << "Enemy Created" << std::endl;
     initGraphics("Enemy.png");
     setPosition(sf::Vector2f(300.f, 100.f));
+	sprite.setScale(currentResolution.width / 1920.0, currentResolution.height / 1080.0);
 }
 
 void Enemy::update()
