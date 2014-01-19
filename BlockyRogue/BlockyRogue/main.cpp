4c171a24f5579c8efc5a3a1f43c1f1c025e52be3
Chris Berizko <cberizko@gmail.com>
2014-01-18 16:03:18 -0800

    basic projectile added

--- BlockyRogue/BlockyRogue/main.cpp
+++ BlockyRogue/BlockyRogue/main.cpp
@@ -1,7 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include "Player.h"
 #include "Enemy.h"
+#include "Projectile.h"
 #include <iostream>
+#include <list>
 using namespace sf;
 
 int main()
@@ -14,9 +16,11 @@ int main()
     shape.setFillColor(sf::Color::Green);
     
     sf::Texture t;
+	float timeOut = 0.f;
 
     Player *p = new Player();
     Enemy  *e = new Enemy();
+	std::list<Projectile*> projectiles;
 
 	sf::Clock clock;
 	window.setMouseCursorVisible(false);
@@ -30,18 +34,63 @@ int main()
                 window.close();
         }
 
+		if(timeOut > 0)
+			timeOut -= elapsed.asSeconds();
+		else
+		{
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+			{
+				projectiles.push_back(new Projectile(sf::Vector2f(p->getPosition().x + p->getBounds().width, 
+					p->getPosition().y + p->getBounds().height / 2), sf::Vector2f(300, 0)));
+				timeOut = .5f;
+			} else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+			{
+				projectiles.push_back(new Projectile(sf::Vector2f(p->getPosition().x, 
+					p->getPosition().y + p->getBounds().height / 2), sf::Vector2f(-300, 0)));
+				timeOut = .5f;
+			} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+			{
+				projectiles.push_back(new Projectile(sf::Vector2f(p->getPosition().x + p->getBounds().width / 2, 
+					p->getPosition().y), sf::Vector2f(0, -300)));
+				timeOut = .5f;
+			} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+			{
+				projectiles.push_back(new Projectile(sf::Vector2f(p->getPosition().x + p->getBounds().width / 2, 
+					p->getPosition().y + p->getBounds().height), sf::Vector2f(0, 300)));
+				timeOut = .5f;
+			}
+		}
+
 		if(sf::Keyboard::isKeyPressed((sf::Keyboard::Escape)))
 			window.close();
 
+		for (std::list<Projectile*>::iterator it = projectiles.begin(); it != projectiles.end();)
+		{
+			(*it)->update(elapsed.asSeconds());
+			if((*it)->dead)
+			{
+				delete *it;
+				it = projectiles.erase(it);
+			}
+			else
+			{
+				++it;
+			}
+		}
 		p->update(elapsed.asSeconds()); 
         e->update();
+
+		for (std::list<Projectile*>::iterator it = projectiles.begin(); it != projectiles.end();++it)
+		{
+			(*it)->draw(&window);
+		}
         p->draw(&window);
         e->draw(&window);
 
 		view.setCenter(sf::Vector2f(p->getPosition().x + p->getBounds().width / 2, p->getPosition().y + p->getBounds().height / 2));
 		window.setView(view);
         window.display();
-        window.clear(Color(255, 255, 255, 255));
+        window.clear(Color(0, 0, 0, 255));
     }
 
     return 0;
