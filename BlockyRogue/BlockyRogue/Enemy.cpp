c5544cc0d35c32cbce14e0f5741f9c8d65ce89c8
MoreJ <Kafke.g@gmail.com>
2014-01-20 15:44:41 -0800

    Added Enemy spawning.

--- BlockyRogue/BlockyRogue/Enemy.cpp
+++ BlockyRogue/BlockyRogue/Enemy.cpp
@@ -4,7 +4,6 @@
 Enemy::Enemy(sf::Vector2f v2f)
 {
 	sf::VideoMode currentResolution = sf::VideoMode::getDesktopMode();
-    std::cout << "Enemy Created" << std::endl;
     initGraphics("Enemy.png");
     setPosition(v2f);
     sprite.setScale(currentResolution.width / 1920.0, currentResolution.height / 1080.0);
--- BlockyRogue/BlockyRogue/EnemyManager.cpp
+++ BlockyRogue/BlockyRogue/EnemyManager.cpp
@@ -3,6 +3,9 @@
 
 EnemyManager::EnemyManager()
 {
+    maxEnemies = config["ENEMYMANAGER_MAX_ENEMIES"];
+    despawnRange = config["ENEMYMANAGER_DESPAWN_RANGE"];
+    srand(time(NULL));
 }
 
 void EnemyManager::update(Player player, float elapsedTime)
@@ -13,7 +16,7 @@ void EnemyManager::update(Player player, float elapsedTime)
     }
     
     despawn(player);
-    spawn();
+    spawn(player);
 }
 
 void EnemyManager::draw(sf::RenderWindow* window)
@@ -35,29 +38,28 @@ void EnemyManager::despawn(Player player)
     sf::VideoMode currentResolution = sf::VideoMode::getDesktopMode();
     int sWidth = currentResolution.width;
     int sHeight = currentResolution.height;
-    int range = 1;
     
     int viewX = player.getPosition().x;
     int viewY = player.getPosition().y;
     
     for (std::list<Enemy*>::iterator it = enemies.begin(); it != enemies.end();)
     {
-        if((*it)->getPosition().x-viewX >= (sWidth/2)*range)
+        if((*it)->getPosition().x-viewX >= (sWidth/2)*despawnRange)
         {
             delete *it;
             it = enemies.erase(it);
         }
-        else if((*it)->getPosition().x-viewX <= (sWidth/2)*(-1*range))
+        else if((*it)->getPosition().x-viewX <= (sWidth/2)*(-1*despawnRange))
         {
             delete *it;
             it = enemies.erase(it);
         }
-        else if((*it)->getPosition().y-viewY >= (sHeight/2)*range)
+        else if((*it)->getPosition().y-viewY >= (sHeight/2)*despawnRange)
         {
             delete *it;
             it = enemies.erase(it);
         }
-        else if((*it)->getPosition().y-viewY <= (sHeight/2)*(-1*range))
+        else if((*it)->getPosition().y-viewY <= (sHeight/2)*(-1*despawnRange))
         {
             delete *it;
             it = enemies.erase(it);
@@ -69,7 +71,32 @@ void EnemyManager::despawn(Player player)
     }
 }
 
-void EnemyManager::spawn()
+void EnemyManager::spawn(Player player)
 {
+    if(enemies.size() < maxEnemies)
+    {
+        sf::VideoMode currentResolution = sf::VideoMode::getDesktopMode();
+        int sWidth = currentResolution.width;
+        int sHeight = currentResolution.height;
+    
+        int viewX = player.getPosition().x;
+        int viewY = player.getPosition().y;
+    
+        int spawnX = viewX;
+        int spawnY = viewY;
+        int x = 0;
+        int y = 0;
+        
+        while((spawnX > viewX-(sWidth/2) && spawnX < viewX+(sWidth/2)) &&
+              (spawnY > viewY-(sHeight/2) && spawnY < viewY+(sHeight/2)))
+        {
+            x = (int)((sWidth/2)*despawnRange)*2;
+            y = (int)((sHeight/2)*despawnRange)*2;
+            spawnX = ((rand() % x)+viewX)-(x/2);
+            spawnY = ((rand() % y)+viewY)-(x/2);
+        }
+
+        enemies.push_back(new Enemy(sf::Vector2f(spawnX, spawnY)));
+    }
     
 }
