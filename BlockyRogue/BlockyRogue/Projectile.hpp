9db1c59aee5dcd1ab9a89b5edd6e6a284ad4fb5d
Justin Britch <jtbritch@gmail.com>
2014-02-09 02:51:37 -0800

    Added bullet death explosions. Tweaked the numbers a bit on bullet range and player speed.

--- BlockyRogue/BlockyRogue/Projectile.hpp
+++ BlockyRogue/BlockyRogue/Projectile.hpp
@@ -12,9 +12,13 @@ class Projectile: public GameObject
     public:
 		enum Direction {LEFT, RIGHT, UP, DOWN};
 		Projectile(sf::Vector2f position, sf::Vector2f velocity, Direction direction);
+		Projectile(sf::Vector2f position);
         void update(float elapsedTime, EnemyManager* EnemyManager);
 		void setVelocity(sf::Vector2f _velocity);
 		bool dead;
+		bool explosion;
+		int frameCount;
+		sf::Vector2f getPosition();
 	protected:
     	sf::Vector2f velocity;
 		sf::Vector2f origin;
