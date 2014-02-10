e098aa9d1597cf4c6dad0f81e4e033c5017cc007
Justin Britch <jtbritch@gmail.com>
2014-02-10 00:38:18 -0800

    Added an aggro range for the enemy squares so we aren't living in a zombie apocalypse.

--- BlockyRogue/BlockyRogue/EnemySquare.cpp
+++ BlockyRogue/BlockyRogue/EnemySquare.cpp
@@ -5,16 +5,27 @@ EnemySquare::EnemySquare(sf::Vector2f v2f, Player* p):Enemy(v2f, p)
 }
 EnemySquare::~EnemySquare()
 {
+	
 }
 void EnemySquare::update(float elapsed)
 {
+	//How far out the enemies can aggro to the player.
+	float aggroRange = 800;
+	
 	sf::Vector2f direction = player->getPosition() - getPosition();
-	float magnitude = sqrt(direction.x * direction.x + direction.y * direction.y);
-	direction.x /= magnitude;
-	direction.y /= magnitude;
-	for(int i = 0; i < shape.getVertexCount(); i++)
-    {
-		shape[i].position += sf::Vector2f(direction.x * moveSpeed*elapsed, direction.y * moveSpeed * elapsed);
+
+	float distanceToPlayer = sqrt(direction.x * direction.x + direction.y * direction.y);
+
+	//If inside aggro range.
+	if (abs(distanceToPlayer) < aggroRange)
+	{
+		float magnitude = distanceToPlayer;
+		direction.x /= magnitude;
+		direction.y /= magnitude;
+		for(int i = 0; i < shape.getVertexCount(); i++)
+		{
+			shape[i].position += sf::Vector2f(direction.x * moveSpeed*elapsed, direction.y * moveSpeed * elapsed);
+		}
 	}
 	Enemy::update(elapsed);
 }
