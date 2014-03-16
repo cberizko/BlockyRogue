fa3837f8c4a449d43f3bc90335d6f2fb0c99169e
Adam L. More <AdamLMore@users.noreply.github.com>
2014-03-15 22:09:26 -0700

    Created Dymanic Shapes based on SmartPoints

---
+++ BlockyRogue/BlockyRogue/SmartPoint.hpp
@@ -0,0 +1,24 @@
+#ifndef _SMARTPOINT_HPP
+#define _SMARTPOINT_HPP
+#include <SFML/Graphics.hpp>
+#include <cmath>
+#define PI 3.14159265
+
+class SmartPoint
+{
+    public:
+        SmartPoint(sf::Vector2f p, int v);
+        
+        sf::Vector2f position;
+        int variance;
+        double radianPosition;
+        sf::Vector2f positionOffset;
+        sf::Vector2f velocity;
+
+        void update(sf::Vector2f moveBy);
+
+    private:
+        
+};
+
+#endif
---
+++ BlockyRogue/BlockyRogue/SmartShape.hpp
@@ -0,0 +1,23 @@
+#ifndef _SMARTSHAPE_HPP
+#define _SMARTSHAPE_HPP
+#include <SFML/Graphics.hpp>
+#include "SmartPoint.hpp"
+#include <iostream>
+#include <list>
+
+class SmartShape
+{
+    public:
+        SmartShape(int verts, int radius, int variance);
+        SmartShape(int verts, int radius, int variance, sf::Vector2f p);
+
+        void update(sf::Vector2f moveBy);
+        sf::VertexArray getShape(sf::Color c);
+
+        std::vector<SmartPoint*> points;
+
+    private:
+        sf::VertexArray vertArray;
+};
+
+#endif
