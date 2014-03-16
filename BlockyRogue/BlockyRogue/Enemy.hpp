996d8ede6a64c0f257ef9c3263cc6f4b9ff5c0f4
MoreJ <Kafke.g@gmail.com>
2014-03-16 08:30:12 -0700

    Fixed some warnings.

--- BlockyRogue/BlockyRogue/Enemy.hpp
+++ BlockyRogue/BlockyRogue/Enemy.hpp
@@ -9,7 +9,7 @@ class Enemy: public Character
 {
     public:
 		Enemy(sf::Vector2f v2f, Player* p, EnemyManager* e, float range);
-        ~Enemy();
+        virtual ~Enemy();
         void update(float elapsed);
         void draw(sf::RenderWindow* window);
         void setHealth(double health);
